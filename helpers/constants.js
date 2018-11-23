9c6ed89e22cc973bb865d87451ccf357b8ef7d06
Rotimi Best <rotimiibitoyebest@yahoo.co.uk>
2018-11-23 23:00:56 +0200

    Added day to constants

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,5 +2,8 @@ module.exports = {
     MONTHS: {
         SHORT: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
         LONG: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    }
+    },
+    DAYS: {
+        LONG: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+    },
 }
