68e88f37bd5b40f605a10eb1e8e7c17f483c248a
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2018-11-25 15:12:02 +0200

    Edit welcome message to new user

--- controllers/Start.js
+++ controllers/Start.js
@@ -66,16 +66,7 @@ class StartController extends TelegramBaseController {
           }
         ]
       });
-      $.sendMessage(
-        `Welcome back ${
-          this.nameOfUser
-        } ${wave}.\n\nTo take a group attendance use the /newattendance command`,
-        {
-          reply_markup: JSON.stringify({
-            remove_keyboard: true
-          })
-        }
-      );
+
       return;
     }
 
@@ -92,7 +83,7 @@ class StartController extends TelegramBaseController {
     $.waitForRequest.then(async $ => {
       if ($.message.text === `Yes ${thumbsUp}`) {
         $.sendMessage(
-          `Okay, Thanks ${userName} ${ok}.\n\nTo begin taking attendance you need to create a group. Use the /newgroup command for that.`,
+          `Okay, Thanks ${userName} ${ok}.\n\nI can help you organize your tasks by allowing you create a todolist.\n\nYou can control me with these commands:\n\n/newtodo - add a task to your todolist\n\n/mytodos - all your pending tasks\n/donetodos - all done todos\n\n/categories - manage todos in categories\n\n/help - ask for help\n/feedback - give me your feedback.`,
           {
             reply_markup: JSON.stringify({
               remove_keyboard: true
@@ -106,7 +97,7 @@ class StartController extends TelegramBaseController {
         $.waitForRequest.then(async $ => {
           userName = $.message.text;
           $.sendMessage(
-            `Okay, Thanks ${userName} ${ok}.\n\nTo begin taking attendance you need to create a group. Use the /newgroup command for that.`,
+            `Okay, Thanks ${userName} ${ok}.\n\nI can help you organize your tasks by allowing you create a todolist.\n\nYou can control me with these commands:\n\n/newtodo - add a task to your todolist\n\n/mytodos - all your pending tasks\n/donetodos - all done todos\n\n/categories - manage todos in categories\n\n/help - ask for help\n/feedback - give me your feedback.`,
             {
               reply_markup: JSON.stringify({
                 remove_keyboard: true
