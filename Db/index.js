d20135e545126bc4026c69395b41896b712d54a5
Rotimi Best <rotimiibitoyebest@yahoo.co.uk>
2019-01-02 15:37:40 +0200

    renamed data to .data

--- Db/index.js
+++ Db/index.js
@@ -1,13 +1,13 @@
 const Datastore = require("nedb-promises");
 
 const todosDb = Datastore.create({
-  filename: "data/todos.db",
+  filename: ".data/todos.db",
   timestampData: true,
   autoload: true
 });
 
 const userDb = Datastore.create({
-  filename: "data/users.db",
+  filename: ".data/users.db",
   timestampData: true,
   autoload: true
 });
