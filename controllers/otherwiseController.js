a6dd435e4c34e7b16df2ec7d78308066f4806fab
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2019-01-19 05:34:57 +0200

    update otherwise controler

--- controllers/otherwiseController.js
+++ controllers/otherwiseController.js
@@ -1,24 +1,27 @@
 const { log } = console;
 const { TelegramBaseController } = require("telegram-node-bot");
-const { COMMANDS } = require('../helpers/constants');
+const { COMMANDS } = require("../helpers/constants");
 const { sendToAdmin } = require("../modules");
 
 class OtherwiseController extends TelegramBaseController {
-    handle($) {
-        const msg = $.message.text;
-        const id = $.message.chat.id;
-        const userName = $.message.chat.firstName
-                        ? $.message.chat.firstName
-                        : $.message.chat.lastName;
+  handle($) {
+    const msg = $.message.text;
+    // const telegramId = $.message.chat.id;
+    const userName = $.message.chat.firstName || $.message.chat.lastName;
 
-        $.sendMessage(`I am a robot and didn't quite understand what you said.${COMMANDS}`, {
-            reply_markup: JSON.stringify({
-              remove_keyboard: true
-            })
-        });
+    $.sendMessage(
+      `I am a robot and didn't quite understand what you said.${COMMANDS}`,
+      {
+        reply_markup: JSON.stringify({
+          remove_keyboard: true
+        })
+      }
+    );
 
-        sendToAdmin(`A user: ${userName} sent this to the bot ${msg || 'Sticker or emoji'}`);
-    }
+    sendToAdmin(
+      `A user: ${userName} sent this to the bot ${msg || "Sticker or emoji"}`
+    );
+  }
 }
 
 module.exports = OtherwiseController;
